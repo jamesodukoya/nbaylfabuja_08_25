c7ed19d601f10573bdd310c6cf6da55e210bd588
jamesodukoya <odukoyaoluwadara@gmail.com>
2025-08-10 11:29:33 -0500

    Added Wrangler and OpenNextJS

---
+++ .open-next/cloudflare/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"NEXT_PUBLIC_WORDPRESS_HOSTNAME":"dash.nbaylfabuja.org","NEXT_PUBLIC_WORDPRESS_URL":"https://dash.nbaylfabuja.org/"};
+export const development = {"NEXT_PUBLIC_WORDPRESS_HOSTNAME":"dash.nbaylfabuja.org","NEXT_PUBLIC_WORDPRESS_URL":"https://dash.nbaylfabuja.org/"};
+export const test = {};
